6f40872b14935d2538be46cae109cd67af919b32
Irum F <irummf@gmail.com>
2018-09-01 23:21:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount=0)
+    total = 0 
+    @total = total 
+    @items = []
+    @discount = discount
+  end
+  
+  def add_item(title, price)
+    @total + title.price
+  end
+end  
