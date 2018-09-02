5b693c0ecb678daece684a87c59cb78a46421c21
Irum F <irummf@gmail.com>
2018-09-02 00:51:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price*quantity 
-    @items << quantity.times do |item|
+    @items << quantity.times { |item|
   end 
   
   def apply_discount
